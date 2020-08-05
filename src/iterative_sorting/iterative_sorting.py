47d9ab8f397cb2811883ad9f6f1676b702985a5b
zacherymoy <49412517+zacherymoy@users.noreply.github.com>
2020-08-05 18:52:40 -0500

    done
    done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,17 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        smallest_val = arr[i]
+        for j in range(i, len(arr)):
+            if arr[j] < smallest_val:
+                smallest_index = j
+                smallest_val = arr[j]
 
         # TO-DO: swap
         # Your code here
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
     return arr
 
@@ -18,29 +25,36 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - (i + 1)):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
 
     return arr
 
 '''
-STRETCH: implement the Counting Sort function below
 
-Counting sort is a sorting algorithm that works on a set of data where
-we specifically know the maximum value that can exist in that set of
-data. The idea behind this algorithm then is that we can create "buckets"
-from 0 up to the max value. This is most easily done by initializing an
-array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
-
-Each buckets[i] then is responsible for keeping track of how many times 
-we've seen `i` in the input set of data as we iterate through it.
-Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
-
-What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
+# STRETCH: implement the Counting Sort function below
+
+# Counting sort is a sorting algorithm that works on a set of data where
+# we specifically know the maximum value that can exist in that set of
+# data. The idea behind this algorithm then is that we can create "buckets"
+# from 0 up to the max value. This is most easily done by initializing an
+# array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
+
+# Each buckets[i] then is responsible for keeping track of how many times 
+# we've seen `i` in the input set of data as we iterate through it.
+# Once we know exactly how many times each piece of data in the input set
+# showed up, we can construct a sorted set of the input data from the 
+# buckets. 
+
+# What is the time and space complexity of the counting sort algorithm?
+# '''
+# def counting_sort(arr, maximum=None):
+#     # Your code here
 
 
-    return arr
+#     return arr
